1e25e32dfa536ff561ce224b2de55cf5249a6bb4
Карим <karim@Karims-MacBook-Pro.local>
2026-03-05 11:36:03 +0300

    beam search vectorized

--- beam_search/beam_search.py
+++ beam_search/beam_search.py
@@ -4,6 +4,7 @@ import heapq
 from typing import Callable
 import heapq
 from itertools import count
+import textwrap
 
 def get_neighbours_vectorized(matrix): # векторизованный вариант получения соседних вершин с данной
     n = matrix.shape[0]
@@ -15,7 +16,7 @@ def get_neighbours_vectorized(matrix): # векторизованный вари
     cnots = []
 
     for i in range(n):
-        cnot_to = [f"{i}"] * n # генерируем заранее известную последовательность cnot-ов
+        cnot_to = [f"{i}"] * n # генерируем заранее известную последовательность cnot-ов (по порядку)
         cnots.append(np.delete((np.array(cnot_from) + np.array(cnot_to)), i))
 
         base = matrix.reshape((1, n, n))
@@ -29,20 +30,19 @@ def get_neighbours_vectorized(matrix): # векторизованный вари
 def beam_search(given_matrix: np.ndarray, beam_width: int, heuristic_func: Callable[[np.ndarray], int]) -> np.ndarray:
     n = given_matrix.shape[0]
     kk = min(n, beam_width)
-    unique_id = count() # чтобы не сравнивались матрицы "внутри" кучи
-
 
     result = [('', given_matrix)] # состоит из пар (path: str, matrix: np.ndarray), где path - путь по CNOT-ам до нее
     # по результатам обработки каждого уровня (kn(n-1) матриц) отбираем топ k матриц и обновляем result
 
-    stop = n ** 3 # заглушка на случай, если что-то пошло не так, чтобы код остановился
+    # stop = n ** 3 # заглушка на случай, если что-то пошло не так, чтобы код остановился
     counter = 0
     working = True
 
     while working:
-        current_top = [(float('-inf'), '', next(unique_id), np.eye(n))] * kk # будем использовать кучу для быстрой обработки максимумов, второй элемент в паре путь для данной матрицы
+        counter += 1
+        current_top = [(float('inf'), '', np.eye(n, dtype=np.uint8))] * kk # будем использовать кучу для быстрой обработки максимумов, второй элемент в паре путь для данной матрицы
         
-        if len(result) == 1: # обработка начала алгоритма
+        if counter == 1: # обработка начала алгоритма
             k = 1
         else:
             k = kk
@@ -51,26 +51,14 @@ def beam_search(given_matrix: np.ndarray, beam_width: int, heuristic_func: Calla
             matrix = result[i][1]
             path = result[i][0]
             cnots, matrices = get_neighbours_vectorized(matrix)
-            for cnot, matr in zip(cnots, matrices):
-                counter += 1 # получаем neighbours этой матрицы, cnot и matr - ребро и вершина соотв
-                val = -heuristic_func(matr)
-                if val == 0 and working:
-                    chars = list(path + cnot) # выводим последовательность cnot
-                    for i in range(0, len(chars), 2):
-                        chars[i], chars[i+1] = chars[i+1], chars[i]
-                    chars = chars[::-1]
-                    res = ""
-                    for i in range(0, len(chars), 2):
-                        res += "".join(chars[i:i+2]) + " "
-                    print(f"{res}")
-                    working = False
-                    break
-                elif len(current_top) < k:
-                    heapq.heappush(current_top, (val, path + cnot, next(unique_id), matr))
-                elif val > current_top[0][0]: # Если текущее число меньше максимума в куче
-                    heapq.heapreplace(current_top, (val, path + cnot, next(unique_id), matr))
-        result = [(item[1], item[3]) for item in current_top]
-
-m = np.array([[0, 1, 1, 0], [1, 1, 1, 0], [1, 0, 1, 0], [1, 0, 0, 1]], dtype=np.uint8)
-beam_search(m, 3, test_heuristic_func.test_heuristic)
-        
+            paths = path + cnots
+            heuristics = test_heuristic_func.test_heuristic(matrices)
+            if np.any(heuristics == 0) and working: # выводим один ответ в красивой форме
+                answer = paths[np.where(heuristics == 0)[0]]
+                print(' '.join(reversed(textwrap.wrap(answer[0], 2))))
+                working = False
+                break
+            triplets = list(zip(heuristics, matrices, paths))
+            top_k = heapq.nsmallest(kk, triplets, key=lambda x: x[0]) # с помощью кучи оставляем только k соседей с наименьшим показателем метрики
+            current_top = heapq.nsmallest(kk, top_k + current_top, key=lambda x: x[0]) # обновляем список кандидатов в k лучших матриц на уровне
+        result = [(item[2], item[1]) for item in current_top] # обновляем уровень
--- beam_search/test_heuristic_func.py
+++ beam_search/test_heuristic_func.py
@@ -1,4 +1,7 @@
 import numpy as np
 
 def test_heuristic(matrix: np.ndarray) -> int:
-    return matrix.sum() - matrix.trace() + (matrix.shape[0] - matrix.trace())
+    sums = matrix.sum(axis=(1, 2))
+    traces = np.diagonal(matrix, axis1=1, axis2=2).sum(axis=1)
+    n = matrix.shape[1]
+    return sums - traces + (n - traces)
